ec62d695f9255909167802bd0d2fd4f8e4038f6a
rajnish-patell <patelrajnish47@gmail.com>
2024-10-24 00:08:53 +0530

    Initial commit

---
+++ basics/test.js
@@ -0,0 +1,18 @@
+let accountId = 008585974;
+let accountEmail = 'patelrajnish47@gmail.com';
+var accountPass = '1234565';
+let accountState;
+
+
+console.log(accountState);
+
+
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPass]);
+
+// {
+//     let accountId = 123456;
+//     console.log(typeof accountId); // Fixed the typo here
+// }
